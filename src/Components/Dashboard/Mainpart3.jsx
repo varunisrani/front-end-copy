f848be1b58f4cd9045e514b679834d2189785117
rcout328 <solovpxoffical@gmail.com>
2024-04-21 02:58:41 +0530

    c

--- src/Components/Dashboard/Mainpart3.jsx
+++ src/Components/Dashboard/Mainpart3.jsx
@@ -9,7 +9,7 @@ export default function Mainpart3() {
       <div className="mad3:mt-10 mad5:mt-20 mad6:mr-[3rem] ">
         <Chat />
       </div>
-      <div className="mt-5 mad1:mt-1 mad2:mt-5 mad3:mt-20 flex justify-center items-center mad5:mt-[8rem]s">
+      <div className="mt-2 mad1:mt-1 mad2:mt-5 mad3:mt-7 flex justify-center items-center mad5:mt-[8rem]">
         <Input />
       </div>
     </div>

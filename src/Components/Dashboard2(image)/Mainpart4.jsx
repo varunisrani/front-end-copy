8271c5b9047e53657bfeb7774ddfe6e569dc850d
rcout328 <solovpxoffical@gmail.com>
2024-04-21 02:49:37 +0530

    c

--- src/Components/Dashboard2(image)/Mainpart4.jsx
+++ src/Components/Dashboard2(image)/Mainpart4.jsx
@@ -9,7 +9,7 @@ export default function Mainpart4() {
       <div className="mad3:mt-5 mad5:mt-[1rem] ">
         <ImageChat1 />
       </div>
-      <div className="mt-[14rem] mad6:mt-[12rem]  mad1:mt-[11rem] mad2:mt-[12rem] mad3:mt-[18rem] mad:mr-10 flex justify-center items-center mad5:mt-[24rem]">
+      <div className="mt-[12rem] mad6:mt-[12rem]  mad1:mt-[11rem] mad2:mt-[12rem] mad3:mt-[18rem] mad:mr-10 flex justify-center items-center mad5:mt-[24rem]">
         <Input />
       </div>
     </div>
